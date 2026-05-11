2c69f81e8289dc9318671239e1387a3554a105ee
zignaggo <ziggnago@gmail.com>
2026-05-11 02:01:03 -0300

    feat: basepath config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'node:path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "node:path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: process.env.BASE_PATH ?? "/",
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
